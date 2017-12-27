6b12bf3836f848a9451dcc76c8d13a4aef3998a2
stormning <stormning@163.com>
2017-12-27 10:47:04 +0800

    query

--- slyak-web/src/main/java/com/slyak/support/freemarker/SlyakRequestContext.java
+++ slyak-web/src/main/java/com/slyak/support/freemarker/SlyakRequestContext.java
@@ -1,8 +1,12 @@
 package com.slyak.support.freemarker;
 
 import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.support.RequestContext;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.web.util.UrlPathHelper;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
@@ -13,15 +17,26 @@ import java.util.Map;
  * @author stormning 2017/12/25
  * @since 1.3.0
  */
-public class SlyakRequestContext {
+public class SlyakRequestContext extends RequestContext {
+    private static final UrlPathHelper urlPathHelper = new UrlPathHelper();
     private HttpServletRequest request;
 
     public SlyakRequestContext(HttpServletRequest request) {
         this.request = request;
     }
 
-    public String query(Map<String, ?> params) {
-        ServletUriComponentsBuilder builder = ServletUriComponentsBuilder.fromRequest(request);
+    public String query(String url, Map<String, ?> params) {
+        if (StringUtils.isEmpty(url)) {
+            //placeholder
+            //foo/{bar}?spam={spam}
+            return getContextUrl(url, params);
+        } else {
+            return replaceCurrentQuery(params);
+        }
+    }
+
+    private String replaceCurrentQuery(Map<String, ?> params) {
+        UriComponentsBuilder builder = ServletUriComponentsBuilder.fromRequest(request);
         if (!CollectionUtils.isEmpty(params)) {
             for (Map.Entry<String, ?> entry : params.entrySet()) {
                 builder.replaceQueryParam(entry.getKey(), entry.getValue());
@@ -62,6 +77,10 @@ public class SlyakRequestContext {
                 .hasNext(hasNext)
                 .hasPrevious(hasPrevious)
                 .build();
+    }
+
+    public String getContextPath() {
+        return this.urlPathHelper.getOriginatingContextPath(this.request);
     }
 
     public static void main(String[] args) {
--- slyak-web/src/main/resources/com/slyak/support/freemarker/slyak.ftl
+++ slyak-web/src/main/resources/com/slyak/support/freemarker/slyak.ftl
@@ -1,3 +1,6 @@
 <#ftl strip_whitespace=true>
 <#-- @ftlvariable name="slyakRequestContext" type="com.slyak.support.freemarker.SlyakRequestContext" -->
-<#macro query extra>${slyakRequestContext.query(extra)}</#macro>
+<#--attributes for dom-->
+<#macro attributes values={}><#list values?keys as k><#if k_index gt 0> </#if>${k}="${values[k]}"</#list></#macro>
+<#--current query and replace param with extra map-->
+<#macro query url="" extra={}>${slyakRequestContext.query(url,extra)}</#macro>
